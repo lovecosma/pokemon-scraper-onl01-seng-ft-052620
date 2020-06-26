cdba735b2a44978faae1bdf2924464c55c9ac767
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-26 15:56:46 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   attr_accessor :name, :type
   attr_reader :id
 
-  def initialize(row_from_db)
+  def initialize(id: 1, name: "Pikachu", type: "electric")
 
     @name = row_from_db[0]
     @type = row_from_db[1]
